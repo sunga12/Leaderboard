bc8b5ba2000189fefd646cb060242554d199d302
Sunga Thawethe <sungabt@gmail.com>
2023-05-16 17:36:49 +0200

    Setup linters

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
-import _ from 'lodash';
 import './index.css';
 import './index.html';
 
 const scores = [
   {
     name: 'Sunga',
-    score: 100
+    score: 100,
   },
   {
     name: 'Felix',
-    score: 90
+    score: 90,
   },
   {
     name: 'Christian',
-    score: 80
+    score: 80,
   },
   {
     name: 'Salim',
-    score: 70
+    score: 70,
   },
   {
     name: 'Otmane',
-    score: 60
+    score: 60,
   },
   {
     name: 'Abdelaziz',
-    score: 50
-  }
+    score: 50,
+  },
 ];
 
 const scoreboard = document.querySelector('.scores');
 
 scores.forEach((entry) => {
-
-    const score = document.createElement('li');
-    score.insertAdjacentHTML('beforeend', `${entry.name} : ${entry.score}`);
-    scoreboard.appendChild(score);
-
+  const score = document.createElement('li');
+  score.insertAdjacentHTML('beforeend', `${entry.name} : ${entry.score}`);
+  scoreboard.appendChild(score);
 });
