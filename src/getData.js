7355d38ba649cf50ee4d76c949d577ba0773add8
Sunga Thawethe <sungabt@gmail.com>
2023-05-18 07:23:23 +0200

    Add additional styles

--- src/getData.js
+++ src/getData.js
@@ -3,15 +3,15 @@ export default async function getTheData() {
   scoreboard.textContent = '';
 
   const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RP7P3DgAooG12Jvz9hXa/scores')
-    .then((res) =>  res.json())
+    .then((res) => res.json())
     .then((data) => {
       data.result.forEach((entry) => {
         const score = document.createElement('li');
-        score.textContent = `${entry.user} : ${entry.score}`;
+        score.className = 'score';
+        score.textContent = `${entry.user}: ${entry.score}`;
         scoreboard.appendChild(score);
       });
     });
 
   return response;
-
 }
--- src/postData.js
+++ src/postData.js
@@ -14,6 +14,7 @@ export default async function postTheData() {
     body: JSON.stringify(data),
   });
 
+  scoreForm.reset();
   getTheData();
 
   return response.json();
