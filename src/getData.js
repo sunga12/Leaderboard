e2b2d4ae339ca1daa682886851144371421d8411
Sunga Thawethe <sungabt@gmail.com>
2023-05-18 06:44:04 +0200

    Add styling

--- src/getData.js
+++ src/getData.js
@@ -3,7 +3,7 @@ export default async function getTheData() {
   scoreboard.textContent = '';
 
   const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RP7P3DgAooG12Jvz9hXa/scores')
-    .then((res) => res.json())
+    .then((res) =>  res.json())
     .then((data) => {
       data.result.forEach((entry) => {
         const score = document.createElement('li');
@@ -13,4 +13,5 @@ export default async function getTheData() {
     });
 
   return response;
+
 }
